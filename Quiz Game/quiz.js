6f04e4a50691f7aafd731d4bedb1f7dab97f34b9
divyanp95 <51528505+divyanp95@users.noreply.github.com>
2019-09-14 18:18:57 +0530

    Add files via upload

---
+++ Quiz Game/quiz.js
@@ -0,0 +1,12 @@
+
+function check(){
+    var ans = document.getElementById("1");
+    if(ans.checked==true)
+    {
+        alert("CORRCT ANSWER");
+    }
+    else{
+        alert("SORRY..TRY AGAIN");
+
+    }
+}
